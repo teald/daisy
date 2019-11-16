8cba8d77906b2ac012bb221f5f6484bf5514f7ea
benhord <benjhord@gmail.com>
2019-11-16 17:59:45 -0500

    PRAISE TUBE

---
+++ tube.py
@@ -0,0 +1,31 @@
+'''
+This file defines the TUBE.
+
+Bow down to the TUBE and it may show mercy upon your soul.
+'''
+
+import numpy as np
+import healpy as hp
+import matplotlib.pyplot as plt
+
+import constants as c
+from daisy import Daisy
+from parcel import Parcel
+
+class Tube(Daisy, Parcel):
+    '''
+    This contains the Tube class.
+    '''
+
+    def setupGrid(self, n):
+        self.n = n
+        self.npix = hp.nside2npix(self.n)
+
+    def __update(self, ):
+        '''
+        Update Parcel parameters for grid.
+        '''
+        #do Parcel stuff
+
+
+#if parcel is on top or bottom then just pass temp from edge as temp in parcel
