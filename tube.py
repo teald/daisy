98b3d2193155471b7a0c374a6b77cef31324312c
benhord <benjhord@gmail.com>
2019-11-22 22:06:43 -0500

    crudely structured code

--- tube.py
+++ tube.py
@@ -25,26 +25,44 @@ class Tube(Parcel):
         self.maxtheta = maxtheta #max latitude to use
         self.deltheta = self.maxtheta / (0.5 * m) #theta resolution
         self.npix = n * m
-        gridpos = np.empty([m,n])
-
-        thetas =  #array like gridpos
+        self.gridpos = np.empty([m,n]) #starting temperatures (to be fixed)
+        self.thetas = np.ones([m,n]) * np.linspace(maxtheta, -maxtheta, m) #array like gridpos (to be fixed)
 ##self.gridpos = np.
         #self.npix = hp.nside2npix(self.n)
         #self.grid = np.arange(npix)
 
-    def __update(self, ):
+    def __update(self, grid):
         '''
         Update Parcel parameters for grid.
         '''
-        for i in self.grid:
-            continue
+        for m,n in self.grid: #go left to right (West to East)
+            #change to for loop with arrays
+            if m = 0:
+                T_t = grid[m,n]
+            else:
+                T_t = grid[m-1,n]
+            if m = len(grid[:,n]) - 1:
+                T_b = grid[m,n]
+            else:
+                T_b = grid[m+1,n]
+            if n = 0:
+                T_l = grid[m,-1:]
+            else:
+                T_l = grid[m,n-1]
+            if n = len(grid[m,:]) - 1:
+                T_r = grid[m,0]
+            else:
+                T_r = grid[m,n+1]
+
+            grid[i,j] = Parcel(args)
         # Do Parcel stuff
-        pass
+        
 
 
 # If parcel is on top or bottom then just pass temp from edge as temp in parcel
 
     def plot(self):
         #plot the grid
-        hp.cartview(self.grid)
-        pass
+        #hp.cartview(self.grid)
+        plt.imshow(self.grid)
+        plt.grid()
